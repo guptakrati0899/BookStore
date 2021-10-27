3833808142e66d5d3307c54dc0f4d945d76e734f
KratiGupta <kratigupta8545@gmail.com>
2021-10-27 17:07:47 +0530

    [Krati]Refactored: Code for some changes in UI

--- pages/Signup/Signup.jsx
+++ pages/Signup/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import '../Signup/Signup.scss'
 
 
