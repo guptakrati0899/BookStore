10370aca1e52d1ee813a2d18c0fd523da33694b1
KratiGupta <kratigupta8545@gmail.com>
2021-10-27 15:12:57 +0530

    [Krati]Added: Routing

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Dashboard from "../src/pages/Dashboard/Dashboard"
+import Signup from './pages/Signup/Signup';
+import {
+  BrowserRouter,Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+      < Switch>
+        <Route path="/Dashboard" component={Dashboard} />
+        
+      </Switch>
+    </BrowserRouter>
+
     </div>
   );
 }
