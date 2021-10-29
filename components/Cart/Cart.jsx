242800b78d97a7bdcfc9eae32ca081230c800e5f
KratiGupta <kratigupta8545@gmail.com>
2021-10-30 02:20:03 +0530

    [Krati]Refactored: Code for adding validation

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -4,12 +4,11 @@ import "../Cart/Cart.scss"
 import Button from '@material-ui/core/Button';
 import Footer from '../Footer/Footer'
 import TextField from '@material-ui/core/TextField';
-import Checkbox from '@material-ui/core/Checkbox';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+import Image from "../../Assets/bookimage.png"
 import { Link } from 'react-router-dom';
 
 
@@ -32,9 +31,47 @@ export class Cart extends Component {
         this.state = {
             open: false,
             openContent: false,
+            Name : "",
+            Number : "",
+            PinCode : "",
+            Locality:"",
+            Address: "",
+            City: "",
+            State:"",
+            nameError: false,
+            numberError:false,
+            pinError:false,
+            locError:false,
+            addError:false,
+            cityError:false,
+            stateError:false,
+
+
+
+
            
           
         }
+    }
+
+
+      
+    isValidated = () => {
+        let isError = false;
+        const errors = this.state;
+        errors.nameError= this.state.Name !=='' ? false : true;
+        errors.numberError= this.state.Number !=='' ? false : true;
+        errors.cityError= this.state.City !=='' ? false : true;
+        errors.pinError= this.state.PinCode !=='' ? false : true;
+        errors.addError= this.state.Address !=='' ? false : true;
+        errors.locError= this.state.Locality !=='' ? false : true;
+        errors.stateError= this.state.State !=='' ? false : true;
+      
+        this.setState({
+            ...errors
+        })
+
+        return isError =errors.nameError || errors.numberError || errors.pinError || errors.addError || errors.locError || errors.stateError || errors.cityError 
     }
 
    
@@ -49,17 +86,47 @@ export class Cart extends Component {
     }
 
     handleContinue = () => {
-        this.setState({ openContent: true });
+        var isValid = this.isValidated();
+        if(!isValid) {
+            console.log("Validation Sucessfull!");
+            this.setState({ openContent: true });
+
     }
+    
+}
+
+change = (e) => {
+    console.log(e.target.value);
+    this.setState({
+        [e.target.name] : e.target.value
+    });
+}
+
 
     
     render() {
         return (
             <div>
-                <Header  info={this.info} displayBook ={this.props.displayBook}/>
+                <Header/>
                 <div className="CartBag-frame">
                     <div className="cartBag-content">
                         <div >My Cart</div>
+                        <div>
+                        <img className="img-book" src={Image} alt="" />
+                    </div>
+                    <div className="text-content">
+                                <div className="bag-text">
+                                    <div className="cart-title">Don't Make Me Think</div>
+                                    <div className="cart-bookAuthor">by Steve Kurg</div>
+                                    <div className="price">Rs. 1500</div>
+                                </div>
+                                <div className="count-content">
+                                    <div className="minus">-</div>
+                                    <div className="count">1</div>
+                                    <div className="plus">+</div>
+                                    <div className="remove">Remove</div>
+                                </div>
+                            </div>
                         <div className="btn-content">
                             <Button variant="contained" className="btn-place"  onClick={this.handleClick} >
                                 Place Order
@@ -83,8 +150,11 @@ export class Cart extends Component {
                                         size="small"
                                         label="FullName"
                                         type="text"
-                                        name="FullName"
+                                        name="Name"
                                         variant="outlined"
+                                        error = {this.state.nameError}
+                                        onChange = {e => this.change(e)}
+                                        helperText = {this.state.nameError ? "Enter a Name" : ''} 
                                         
                                     />
                                     </div>
@@ -97,6 +167,10 @@ export class Cart extends Component {
                                         type="text"
                                         name="Number"
                                         variant="outlined"
+                                        error = {this.state.numberError}
+                                        onChange = {e => this.change(e)}
+                                        helperText = {this.state.numberError ? "Enter a Number" : ''} 
+                                        
                                       
                                     /></div>
                                 </div>
@@ -106,10 +180,14 @@ export class Cart extends Component {
                                     <TextField
                                      
                                         size="small"
-                                        label="Pin Code"
+                                        label="PinCode"
                                         type="text"
                                         name="PinCode"
                                         variant="outlined"
+                                        error = {this.state.pinError}
+                                        onChange = {e => this.change(e)}
+                                        helperText = {this.state.pinError ? "Enter a Pincode" : ''} 
+                                        
                                        
                                     />
                                 </div>
@@ -121,6 +199,9 @@ export class Cart extends Component {
                                         type="text"
                                         name="Locality"
                                         variant="outlined"
+                                        error = {this.state.locError}
+                                        onChange = {e => this.change(e)}
+                                        helperText = {this.state.locError ? "Enter a Locality" : ''} 
                                       
                                     />
                                 </div>
@@ -133,6 +214,9 @@ export class Cart extends Component {
                                 name="Address"
                                 variant="outlined"
                                 size="large"
+                                error = {this.state.addError}
+                                onChange = {e => this.change(e)}
+                                helperText = {this.state.addError ? "Enter an Address" : ''} 
                             
                                 fullWidth
                               
@@ -148,6 +232,9 @@ export class Cart extends Component {
                                         type="text"
                                         name="City"
                                         variant="outlined"
+                                        error = {this.state.cityError}
+                                        onChange = {e => this.change(e)}
+                                        helperText = {this.state.cityError ? "Enter a City/Town" : ''} 
                                        /></div>
                                 </div>
                                 <div className="state">
@@ -158,6 +245,9 @@ export class Cart extends Component {
                                         type="text"
                                         name="State"
                                         variant="outlined"
+                                        error = {this.state.stateError}
+                                        onChange = {e => this.change(e)}
+                                        helperText = {this.state.stateError ? "Enter a LandMark" : ''} 
                                         
                                     /></div>
                                 </div>
