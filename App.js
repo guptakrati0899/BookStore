012c8a9712f8806af78e9c5b2e8c2cfcccd2b860
KratiGupta <kratigupta8545@gmail.com>
2021-10-29 14:44:30 +0530

    [Krati]Refactored: Code for some changes in the routing

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Dashboard from "../src/pages/Dashboard/Dashboard"
-import{BrowserRouter as Router , Switch,Route} from 'react-router-dom';
+import{BrowserRouter as Router , Switch,Route,Redirect} from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 function App() {
@@ -9,10 +9,11 @@ function App() {
     <div className="App">
      <Router>
       < Switch>
-        <Route path ="/Login" component={Dashboard} />
-        <Route path="/BooksDashboard" component={Home} />
-
-        
+        < Route exact path ="/">
+        <Redirect to = "/dashboard"/>
+        </Route>
+        <Route  path ="/dashboard" component={Dashboard} />
+        <Route  path="/home" component={Home} />
       </Switch>
     </Router> 
 
